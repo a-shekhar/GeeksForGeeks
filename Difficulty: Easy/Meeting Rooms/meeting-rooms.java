f4116ecb627f98494a9f8f4d69c7c84d7747c94f
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-07-28 22:33:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,13 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        
+        // code here
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0],b[0])); // sort based on start time
+        for(int i = 1; i < arr.length; i++){
+         if(arr[i][0] < arr[i-1][1]){
+            return false;
+         }
+       }
+        return true;
+    }
+}
