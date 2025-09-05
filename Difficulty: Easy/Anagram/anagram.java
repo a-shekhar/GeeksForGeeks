fecfc564b87e847bcc0a6a8000155e422c931a07
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-09-05 22:27:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,47 +1,24 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-import java.util.stream.*;
-
-class GFG {
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String str = br.readLine();
-            String s1 = str.split(" ")[0];
-            String s2 = str.split(" ")[1];
-
-            Solution obj = new Solution();
-
-            if (obj.isAnagram(s1, s2)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    
-    public static String sort(String str){
-        char[] chars = str.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
-    }
-    
-    
-    // Function is to check whether two strings are anagram of each other or not.
-    public static boolean isAnagram(String str1, String str2) {
+    public static boolean areAnagrams(String str1, String str2) {
         if(str1.length() != str2.length()){
             return false;
         }
-        return sort(str1).equals(sort(str2));
+        
+        int[] arr = new int[26];
+        int val1, val2;
+        for(int i = 0; i < str1.length(); i++){
+            val1 = str1.charAt(i) - 'a';
+            val2 = str2.charAt(i) - 'a';
+            arr[val1]++;
+            arr[val2]--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
