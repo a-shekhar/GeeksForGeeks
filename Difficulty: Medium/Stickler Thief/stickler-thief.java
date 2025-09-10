76a08ae7f9febef2a6065d72c5d0098233d1f922
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-09-10 23:49:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.java
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -1,50 +1,21 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.io.*;
-
-class GFG
- {
-	public static void main (String[] args)
-	 {
-	  
-	  //taking input using Scanner class
-	  Scanner sc = new Scanner(System.in);
-	  
-	  //taking count of testcases
-	  int t = sc.nextInt();
-	  while(t-- > 0){
-	      
-	      //taking count of houses
-	      int n = sc.nextInt();
-	      int arr[] = new int[n];
-	      
-	      //inserting money present in 
-	      //each house in the array
-	      for(int i = 0; i<n; ++i)
-	           arr[i] = sc.nextInt();
-  	      
-  	      //calling method FindMaxSum() of class solve
-  	      System.out.println(new Solution().FindMaxSum(arr, n));
-	 }
-   }
-}
-// } Driver Code Ends
-
-
-class Solution
-{
-    //Function to find the maximum money the thief can get.
-    public int FindMaxSum(int nums[], int n)
-    {
-        if(nums.length == 1){
-            return nums[0];
+class Solution {
+    public int findMaxSum(int arr[]) {
+        if(arr.length == 1){
+            return 0;
         }
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i < nums.length; i++){
-            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+
+        if(arr.length == 2){
+            return Math.max(arr[0], arr[1]);
         }
-        return dp[n - 1];
+        
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+        for(int i = 2; i < arr.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i]);
+        }
+
+        return dp[arr.length - 1];
+        
     }
 }
