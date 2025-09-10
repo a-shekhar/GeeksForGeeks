1fde9563ebd47392e0dd4ef172efdf9acccee919
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-09-10 23:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count ways to express N as the sum of 1,3 and 4/count-ways-to-express-n-as-the-sum-of-13-and-4.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+    public int countWays(int n) {
+       if(n <= 2){
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        for(int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 3] + dp[i - 4];
+        }
+
+        return dp[n];
+    }
+};
