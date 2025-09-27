cce5c6557884b6be4faf53fda972414f00796290
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-09-27 12:11:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 5/pattern-5.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }  
+    }
+}
