4aabd0af20df601aa1fd676604d7d8888c4446a6
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-07 22:30:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.java
+++ Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.java
@@ -2,46 +2,29 @@ class Solution {
 
     public String encode(String s[]) {
         StringBuilder builder = new StringBuilder();
-        for (String str: s){
-            builder.append(str.length());
-            builder.append("#");
-            builder.append(str);
+        for(String str : s){
+            builder.append(str.length()).append("#").append(str);
         }
         return builder.toString();
     }
 
     public String[] decode(String s) {
-        if(s.trim().length() == 0){
-            return new String[]{s};
-        }
-        int i = 0;
-        int j;
-        List<String> strList = new ArrayList<>();
-        
-
-        while (i < s.length()) {
+        ArrayList<String> list = new ArrayList<>();
+        int i = 0; 
+        int j = 0;
+        while(i < s.length()){
             j = i;
-            while(s.charAt(j) != '#'){ // when it is not equal to #
+            while(s.charAt(j) != '#'){
                 j++;
             }
 
-            int length = Integer.parseInt(s.substring(i, j)); // read actual length
-
-            // read next string 
-            String word = s.substring(j+1, j+1+length); // next word
-            strList.add(word);
-
-            i = j + 1 + length;
+            int len = Integer.parseInt(s.substring(i, j));
+            String word = s.substring(j+1, j+1+len);
+            list.add(word);
+            // move to next segment
+            i = j + 1 + len;
         }
-        
-        String[] arr = new String[strList.size()];
-            int k = 0;
-            
-            for(String str : strList){
-                arr[k] = str;
-                k++;
-            }
 
-            return arr;
+        return list.toArray(new String[0]);
     }
 }
