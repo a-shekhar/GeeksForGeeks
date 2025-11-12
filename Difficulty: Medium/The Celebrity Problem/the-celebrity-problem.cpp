b22d92f150bb3c96961b8bcb90e2bddc2954b06a
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-11-12 11:45:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+   if(n == 0) return -1;
+
+
+   int cand = 0 ;
+   for(int i = 1 ; i < n ;i++){
+       if(mat[cand][i] == 1) cand = i;
+   }
+
+
+   for(int i = 0 ; i < n; i++){
+       if(i == cand) continue;
+       if(mat[cand][i] == 1) return -1;
+       if(mat[i][cand] == 0 ) return -1;
+   }
+
+
+   return cand;
+    }
+};
